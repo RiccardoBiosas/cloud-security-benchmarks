9e8948f26732f983fc7ca6461daf6a3cb24105c1
lain <rbios@protonmail.com>
2023-04-01 08:51:42 +0200

    feat: add checkov static analyzer

---
+++ iac-package/checkov_runner/main.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+from checkov.terraform.runner import Runner
+
+repo_root = Path(__file__).resolve().parent.parent.parent
+target_directory =  f"{repo_root}/terragoat"
+
+checkov_runner = Runner()
+checkov_output = checkov_runner.run(
+    root_folder=target_directory,
+    external_checks_dir="",
+    files=[target_directory],
+)
+
+checkov_results = checkov_output.get_dict()["results"]
+print(len(checkov_results["failed_checks"]))
+
+for check in checkov_results["failed_checks"]:
+    print(check)
